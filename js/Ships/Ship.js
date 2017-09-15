fbd1dab6de609983408e76d522c9dc4ceadf2bde
jason.smith <jason.smith@fleetmatics.com>
2017-09-15 17:34:12 +0100

    all module types added

--- js/Ships/Ship.js
+++ js/Ships/Ship.js
@@ -7,6 +7,7 @@ SpaceSim.Ships.Ship = function(options) {
   this.cost = options.cost || 0;
 
   this.hull = new SpaceSim.Ships.CoreModules.Hull(options.hull);
+  this.fuelTank = new SpaceSim.Ships.CoreModules.FuelTank(options.fuelTank);
 
   this.coreModules = new SpaceSim.Ships.CoreModules(options.coreModules);
   this.weaponModules = new SpaceSim.Ships.WeaponModules(options.weaponModules);
@@ -52,23 +53,31 @@ SpaceSim.Ships.PoweredModule = function(options) {
 SpaceSim.Ships.PoweredModule.prototype = Object.create(SpaceSim.Ships.Module.prototype);
 SpaceSim.Ships.PoweredModule.prototype.constructor = SpaceSim.Ships.PoweredModule;
 
-SpaceSim.Ships.CoreModules = function(options) {
-  this.generator = new SpaceSim.Ships.CoreModules.Generator(options.generator);
-  this.thrusters = new SpaceSim.Ships.CoreModules.Thrusters(options.thrusters);
-  this.warpCore = new SpaceSim.Ships.CoreModules.WarpCore(options.warpCore);
-  this.lifeSupport = options.lifeSupport || 0;
-  this.capacitor = options.capacitor || 0;
-  this.fuelTank = options.fuelTank || 0;
-};
-
-SpaceSim.Ships.CoreModules.Hull = function(options) {
+SpaceSim.Ships.Hull = function(options) {
   SpaceSim.Ships.Module.call(this, options);
 
   this.thermalResistance = options.thermalResistance || 0;
   this.impactResistance = options.impactResistance || 0;
 };
-SpaceSim.Ships.CoreModules.Hull.prototype = Object.create(SpaceSim.Ships.Module.prototype);
-SpaceSim.Ships.CoreModules.Hull.prototype.constructor = SpaceSim.Ships.CoreModules.Hull;
+SpaceSim.Ships.Hull.prototype = Object.create(SpaceSim.Ships.Module.prototype);
+SpaceSim.Ships.Hull.prototype.constructor = SpaceSim.Ships.Hull;
+
+SpaceSim.Ships.FuelTank = function(options) {
+  SpaceSim.Ships.Module.call(this, options);
+
+  this.boostPower = options.boostPower || 0;
+  this.boostTime = options.boostTime || 0;
+};
+SpaceSim.Ships.FuelTank.prototype = Object.create(SpaceSim.Ships.Module.prototype);
+SpaceSim.Ships.FuelTank.prototype.constructor = SpaceSim.Ships.FuelTank;
+
+SpaceSim.Ships.CoreModules = function(options) {
+  this.generator = new SpaceSim.Ships.CoreModules.Generator(options.generator);
+  this.thrusters = new SpaceSim.Ships.CoreModules.Thrusters(options.thrusters);
+  this.warpCore = new SpaceSim.Ships.CoreModules.WarpCore(options.warpCore);
+  this.lifeSupport = new SpaceSim.Ships.CoreModules.LifeSupport(options.lifeSupport);
+  this.capacitor = new SpaceSim.Ships.CoreModules.Capacitor(options.capacitor);
+};
 
 SpaceSim.Ships.CoreModules.Generator = function(options) {
   SpaceSim.Ships.PoweredModule.call(this, options);
@@ -90,9 +99,48 @@ SpaceSim.Ships.CoreModules.Thrusters.prototype.constructor = SpaceSim.Ships.Core
 SpaceSim.Ships.CoreModules.WarpCore = function(options) {
   SpaceSim.Ships.PoweredModule.call(this, options);
 
-  this.maximumMass = options.maximumMass || 0;
-  this.maximumFuel = options.maximumFuel || 0;
-  this.maximumRange = options.maximumRange || 0;
+  this.maximumMass = options.maximumMass || 0; // maximum mass that can be shifted. above maximum the jump range drops to 0
+  this.maximumFuel = options.maximumFuel || 0; // maximum fuel used for maximum jump
+  this.maximumRange = options.maximumRange || 0; // largest jump range at 0 mass
 };
 SpaceSim.Ships.CoreModules.WarpCore.prototype = Object.create(SpaceSim.Ships.PoweredModule.prototype);
 SpaceSim.Ships.CoreModules.WarpCore.prototype.constructor = SpaceSim.Ships.CoreModules.WarpCore;
+
+SpaceSim.Ships.CoreModules.LifeSupport = function(options) {
+  SpaceSim.Ships.PoweredModule.call(this, options);
+
+  this.time = options.time || 0;
+};
+SpaceSim.Ships.CoreModules.LifeSupport.prototype = Object.create(SpaceSim.Ships.PoweredModule.prototype);
+SpaceSim.Ships.CoreModules.LifeSupport.prototype.constructor = SpaceSim.Ships.CoreModules.LifeSupport;
+
+SpaceSim.Ships.CoreModules.Capacitor = function(options) {
+  SpaceSim.Ships.PoweredModule.call(this, options);
+
+  this.boostPower = options.boostPower || 0;
+  this.boostTime = options.boostTime || 0;
+};
+SpaceSim.Ships.CoreModules.Capacitor.prototype = Object.create(SpaceSim.Ships.PoweredModule.prototype);
+SpaceSim.Ships.CoreModules.Capacitor.prototype.constructor = SpaceSim.Ships.CoreModules.Capacitor;
+
+SpaceSim.Ships.WeaponModules = function(options) {
+  this.smallCount = options.smallCount || 0;
+  this.mediumCount = options.mediumCount || 0;
+  this.largeCount = options.largeCount || 0;
+
+  this.smallModules = [this.smallCount];
+  this.mediumModules = [this.mediumCount];
+  this.largeModules = [this.largeCount];
+};
+
+SpaceSim.Ships.DefenseModules = function(options) {
+  this.count = options.count || 0;
+
+  this.modules = [this.count];
+};
+
+SpaceSim.Ships.UtilityModules = function(options) {
+  this.count = options.count || 0;
+
+  this.modules = [this.count];
+};
