11c5d889cba14dc2ec7bef02f9720e96df23b36d
Jason Holt Smith <bicarbon8@gmail.com>
2022-12-18 19:46:26 +0000

    simplifying build

--- projects/server/webpack.config.cjs
+++ projects/server/webpack.config.cjs
@@ -1,12 +1,10 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env) => {
   return {
     entry: {
-      engine: './src/app/space-sim-game-engine.ts',
-      // server: './src/server.ts'
+      engine: './src/app/space-sim-game-engine.ts'
     },
     devtool: 'source-map',
     module: {
